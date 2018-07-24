ec699b2796a5e3d19beb9feeadc38dc3c03d4e9b
mack1070101 <mackenziebligh@gmail.com>
2018-07-24 13:55:00 -0700

    Stub extension functions used to build strings

--- src/main/kotlin/com/gocopia/repo/DynamoInverter.kt
+++ src/main/kotlin/com/gocopia/repo/DynamoInverter.kt
@@ -1,2 +1,15 @@
 package com.gocopia.repo
 
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator
+
+fun QuerySpec.toSqlString(tableName: String): String {
+
+    return ""
+}
+
+fun ScanSpec.toSqlString(tableName: String): String {
+
+    return ""
+}
