797c95cc0440ba8caa0e66d23d72a9cce3807054
mack1070101 <mackenziebligh@gmail.com>
2018-07-24 12:55:36 -0700

    Stub Testing class

--- src/test/kotlin/com/gocopia/repo/DynamoInverterTest.kt
+++ src/test/kotlin/com/gocopia/repo/DynamoInverterTest.kt
@@ -1,4 +1,20 @@
 package com.gocopia.repo
 
-class DynamoInverterTest {
+import io.kotlintest.specs.FunSpec
+
+/**
+ * Handles testing the DynamoInverter extension functions to ensure that SQL Strings are properly built
+ *
+ * @author
+ */
+class DynamoInverterTest : FunSpec() {
+
+    // Where test functions are called to be ran
+    init {
+
+    }
+
+    fun basicHashKeyTest() = test("Test Query Spec with variety of Hash Keys") {
+
+    }
 }
