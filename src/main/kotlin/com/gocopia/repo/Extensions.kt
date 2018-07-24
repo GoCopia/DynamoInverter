7da22350d5cb228d759bde35b5c3c9353abad79a
mack1070101 <mackenziebligh@gmail.com>
2018-07-24 14:43:12 -0700

    Add internal to escapeResult function so other modules can't see it

--- src/main/kotlin/com/gocopia/repo/Extensions.kt
+++ src/main/kotlin/com/gocopia/repo/Extensions.kt
@@ -3,7 +3,7 @@ package com.gocopia.repo
 /**
  * Local function to escape the result if doing string comparisons and leave it untouched if not
  */
-fun Any.escapeResultIfNeeded(): Any {
+internal fun Any.escapeResultIfNeeded(): Any {
     return when(this::class.java) {
         String::class.java -> "\'$this\'"
         else -> this
