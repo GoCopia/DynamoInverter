f455f5715526a15dcb78fa1063ac99aaa31845f7
mack1070101 <mackenziebligh@gmail.com>
2018-07-24 14:42:31 -0700

    Implement function to escape strings if necessary

--- src/main/kotlin/com/gocopia/repo/Extensions.kt
+++ src/main/kotlin/com/gocopia/repo/Extensions.kt
@@ -1,2 +1,12 @@
 package com.gocopia.repo
 
+/**
+ * Local function to escape the result if doing string comparisons and leave it untouched if not
+ */
+fun Any.escapeResultIfNeeded(): Any {
+    return when(this::class.java) {
+        String::class.java -> "\'$this\'"
+        else -> this
+    }
+}
+
